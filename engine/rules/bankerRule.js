cafb87d3859b64ccea2672012ee0ab3e999c284d
weili-0213 <weili880213@gmail.com>
2026-07-28 21:32:40 +0800

    Add bankerMustDraw function for drawing rules
    Implement banker draw rules based on hand value and player's third card.

--- engine/rules/bankerRule.js
+++ engine/rules/bankerRule.js
@@ -1,1 +1,25 @@
+import { bankerThirdCardRule } from "./thirdCardRule.js";
 
+/**
+ * 莊家補牌規則
+ */
+
+export function bankerMustDraw(
+    hand,
+    playerThirdCard = null
+) {
+
+    if (hand.isNatural) {
+        return false;
+    }
+
+    if (!playerThirdCard) {
+        return hand.value <= 5;
+    }
+
+    return bankerThirdCardRule(
+        hand.value,
+        playerThirdCard
+    );
+
+}
