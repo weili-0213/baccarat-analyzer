4072feaabee0f58d6a3a7211eb914adc39b00dff
weili-0213 <weili880213@gmail.com>
2026-07-28 21:31:23 +0800

    Update playerRule.js

--- engine/rules/playerRule.js
+++ engine/rules/playerRule.js
@@ -1,1 +1,17 @@
+/**
+ * 閒家補牌規則
+ *
+ * Natural 8/9 不補牌
+ * 0~5 補牌
+ * 6~7 停牌
+ */
 
+export function playerMustDraw(hand) {
+
+    if (hand.isNatural) {
+        return false;
+    }
+
+    return hand.value <= 5;
+
+}
