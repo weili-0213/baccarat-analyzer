81afd46caaa4c45ccc78fc806bd491af575d4d3f
weili-0213 <weili880213@gmail.com>
2026-07-28 21:26:35 +0800

    Implement Deck class for Baccarat game
    Added Deck class to model a standard deck of cards with ranks and suits.

--- engine/deck.js
+++ engine/deck.js
@@ -1,1 +1,106 @@
+/**
+ * Baccarat Analyzer
+ * -----------------------------------------
+ * Deck Model
+ *
+ * 一副標準撲克牌
+ *
+ * 52 Cards
+ *
+ * Rank:
+ * A,2,3,4,5,6,7,8,9,10,J,Q,K
+ *
+ * Suit:
+ * S,H,D,C
+ */
+
+
+import Card from "./card.js";
+
+
+const RANKS = [
+
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+
+];
+
+
+const SUITS = [
+
+    "S",
+    "H",
+    "D",
+    "C"
+
+];
+
+
+export default class Deck {
+
+
+    constructor(deckNumber = 1){
+
+    if(
+        !Number.isInteger(deckNumber) ||
+        deckNumber < 1
+    ){
+        throw new Error(
+            `Invalid deck number: ${deckNumber}`
+        );
+    }
+
+
+    this.deckNumber = deckNumber;
+
+    this.cards = [];
+
+    this.create();
+
+} 
+
+
+
+    /**
+     * 建立52張牌
+     */
+    create() {
+
+        this.cards = [];
+
+
+        for(const suit of SUITS){
+
+
+            for(const rank of RANKS){
+
+
+                this.cards.push(
+
+                    new Card(
+                        rank,
+                        suit,
+                        this.deckNumber
+                    )
+
+                );
+
+
+            }
+
+        }
+
+
+    }
 
