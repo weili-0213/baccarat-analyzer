cfc35f85d8e6e2d2c4e2ac5d95932b5b3535f477
weili-0213 <weili880213@gmail.com>
2026-07-31 20:55:26 +0800

    Define DealerState with game states
    Added DealerState object with various states for the dealer.

--- engine/dealerState.js
+++ engine/dealerState.js
@@ -1,1 +1,19 @@
+/**
+ * Dealer State
+ */
 
+const DealerState = Object.freeze({
+
+    READY: "READY",
+
+    INITIAL_DEALT: "INITIAL_DEALT",
+
+    PLAYER_THIRD: "PLAYER_THIRD",
+
+    BANKER_THIRD: "BANKER_THIRD",
+
+    FINISHED: "FINISHED"
+
+});
+
+export default DealerState;
