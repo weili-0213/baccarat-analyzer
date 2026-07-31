e211065baf5fe2b9531d5125dbf669e2ca5d0cec
weili-0213 <weili880213@gmail.com>
2026-07-31 20:50:34 +0800

    Implement Baccarat Dealer class with game logic

--- engine/dealer.js
+++ engine/dealer.js
@@ -1,1 +1,130 @@
+/**
+ * Baccarat Analyzer
+ * -----------------------------------------
+ *
+ * Dealer
+ *
+ * 百家樂發牌引擎
+ *
+ */
 
+import Round from "./round.js";
+
+import {
+    playerMustDraw
+} from "./rules/playerRule.js";
+
+import {
+    bankerMustDraw
+} from "./rules/bankerRule.js";
+
+export default class Dealer {
+
+    constructor(shoe) {
+
+        this.shoe = shoe;
+
+    }
+
+    /**
+     * 抽一張牌
+     */
+    draw() {
+
+        return this.shoe.draw();
+
+    }
+
+    /**
+     * 發一局牌
+     */
+    play() {
+
+        const round = new Round();
+
+        //-------------------------
+        // 初始四張牌
+        //-------------------------
+
+        round.deal(
+            "player",
+            this.draw()
+        );
+
+        round.deal(
+            "banker",
+            this.draw()
+        );
+
+        round.deal(
+            "player",
+            this.draw()
+        );
+
+        round.deal(
+            "banker",
+            this.draw()
+        );
+
+        //-------------------------
+        // Natural
+        //-------------------------
+
+        if (round.isNatural) {
+
+            round.finish();
+
+            return round;
+
+        }
+
+        //-------------------------
+        // Player 第三張
+        //-------------------------
+
+        let playerThird = null;
+
+        if (
+            playerMustDraw(
+                round.playerScore
+            )
+        ) {
+
+            playerThird = this.draw();
+
+            round.deal(
+                "player",
+                playerThird
+            );
+
+        }
+
+        //-------------------------
+        // Banker 第三張
+        //-------------------------
+
+        if (
+            bankerMustDraw(
+                round.bankerScore,
+                playerThird
+            )
+        ) {
+
+            round.deal(
+                "banker",
+                this.draw()
+            );
+
+        }
+
+        //-------------------------
+        // 完成
+        //-------------------------
+
+        round.finish();
+
+        return round;
+
+    }
+
+}
