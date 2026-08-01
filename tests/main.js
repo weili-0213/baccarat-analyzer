e0c36975bff620e02542198d948fbf3044ed9737
weili-0213 <weili880213@gmail.com>
2026-08-01 21:39:14 +0800

    Add async run function to execute tests

--- tests/main.js
+++ tests/main.js
@@ -1,1 +1,35 @@
+async function run() {
 
+    console.clear();
+
+    console.log("==========");
+
+    console.log("Dealer");
+
+    console.log("==========");
+
+    await testDealer();
+
+    console.log("");
+
+    console.log("==========");
+
+    console.log("Round");
+
+    console.log("==========");
+
+    await testRound();
+
+    console.log("");
+
+    console.log("==========");
+
+    console.log("History");
+
+    console.log("==========");
+
+    await testHistory();
+
+}
+
+run();
