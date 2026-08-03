e4a65f9615c203194f3b7dd2aaa690055a6654e5
weili-0213 <weili880213@gmail.com>
2026-08-03 17:45:45 +0800

    Implement createRuntimePipeline function

--- runtime/createRuntimePipeline.js
+++ runtime/createRuntimePipeline.js
@@ -1,1 +1,31 @@
+/**
+ * Baccarat Analyzer V5.5
+ * runtime/createRuntimePipeline.js
+ */
 
+import RuntimePipeline
+    from "./pipeline/RuntimePipeline.js";
+
+
+export const RUNTIME_PIPELINE_FACTORY_VERSION = "5.5.0";
+
+
+export default function createRuntimePipeline({
+    eventBus = null,
+    clock = () => Date.now(),
+    stopOnError = true,
+    stages = []
+} = {}) {
+    const pipeline =
+        new RuntimePipeline({
+            eventBus,
+            clock,
+            stopOnError
+        });
+
+    for (const stage of stages) {
+        pipeline.register(stage);
+    }
+
+    return pipeline;
+}
