8b0324ef45725da01a541b792ba35f4bfa5da53b
weili-0213 <weili880213@gmail.com>
2026-08-03 18:23:03 +0800

    Implement createRuntimeMonitor function

--- runtime/createRuntimeMonitor.js
+++ runtime/createRuntimeMonitor.js
@@ -1,1 +1,31 @@
+/**
+ * Baccarat Analyzer V5.6
+ * runtime/createRuntimeMonitor.js
+ */
 
+import RuntimeMonitor
+    from "./monitor/RuntimeMonitor.js";
+
+
+export const RUNTIME_MONITOR_FACTORY_VERSION = "5.6.0";
+
+
+export default function createRuntimeMonitor({
+    eventBus = null,
+    runtime = null,
+    scheduler = null,
+    pipeline = null,
+    controller = null,
+    clock = () => Date.now(),
+    historyLimit = 100
+} = {}) {
+    return new RuntimeMonitor({
+        eventBus,
+        runtime,
+        scheduler,
+        pipeline,
+        controller,
+        clock,
+        historyLimit
+    });
+}
