720bb9c6f9946515fbab967498cb5d6226c65bca
weili-0213 <weili880213@gmail.com>
2026-08-03 17:32:25 +0800

    Implement createRuntimeScheduler function
    Add createRuntimeScheduler function for task scheduling

--- runtime/createRuntimeScheduler.js
+++ runtime/createRuntimeScheduler.js
@@ -1,1 +1,31 @@
+/**
+ * Baccarat Analyzer V5.4
+ * runtime/createRuntimeScheduler.js
+ */
 
+import RuntimeScheduler
+    from "./scheduler/RuntimeScheduler.js";
+
+
+export const RUNTIME_SCHEDULER_FACTORY_VERSION = "5.4.0";
+
+
+export default function createRuntimeScheduler({
+    eventBus = null,
+    interval = 100,
+    mode = "interval",
+    scheduler = globalThis,
+    clock = () => Date.now(),
+    maxTasksPerTick = 10,
+    contextFactory = null
+} = {}) {
+    return new RuntimeScheduler({
+        eventBus,
+        interval,
+        mode,
+        scheduler,
+        clock,
+        maxTasksPerTick,
+        contextFactory
+    });
+}
