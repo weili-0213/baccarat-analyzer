5fa99f9615f54c2fafa2f00cec49cd6b739a4f02
weili-0213 <weili880213@gmail.com>
2026-08-03 16:44:29 +0800

    Implement createRuntimeControllers function
    Added createRuntimeControllers function to manage runtime and round controllers.

--- runtime/createRuntimeControllers.js
+++ runtime/createRuntimeControllers.js
@@ -1,1 +1,42 @@
+/**
+ * Baccarat Analyzer V5.2
+ * runtime/createRuntimeControllers.js
+ */
 
+import RuntimeController
+    from "./controllers/RuntimeController.js";
+
+import RoundController
+    from "./controllers/RoundController.js";
+
+
+export const RUNTIME_CONTROLLERS_VERSION = "5.2.0";
+
+
+export default function createRuntimeControllers({
+    runtime,
+    requiredCards = 4,
+    cardValidator = null,
+    onStateChange = null,
+    onError = null
+} = {}) {
+    const roundController =
+        new RoundController({
+            requiredCards,
+            validator:
+                cardValidator
+        });
+
+    const runtimeController =
+        new RuntimeController({
+            runtime,
+            roundController,
+            onStateChange,
+            onError
+        });
+
+    return {
+        runtimeController,
+        roundController
+    };
+}
